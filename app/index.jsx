fbe2c05d8354b0f798da68b5a3a194bcfc46888b
franzap <_@franzap.com>
2023-09-06 16:56:20 -0300

    Closes #15

--- app/index.jsx
+++ app/index.jsx
@@ -8,6 +8,7 @@ import ZapThreads from '../src';
 import { shortenEncodedId } from "../src/util/ui"; 
 import { createSignal, createMemo, onCleanup } from 'solid-js';
 import { Select } from "@thisbeyond/solid-select";
+import "@thisbeyond/solid-select/style.css";
 
 const root = document.getElementById('root');
 
@@ -18,16 +19,14 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
 }
 
 const options = [
-  { label: "fiatjaf blog", name: "https://fiatjaf.com/nostr.html" },
   { label: "Gigi article", name: "naddr1qqxnzd3cxqmrzv3exgmr2wfeqgsxu35yyt0mwjjh8pcz4zprhxegz69t4wr9t74vk6zne58wzh0waycrqsqqqa28pjfdhz" },
+  { label: "fiatjaf blog", name: "https://fiatjaf.com/nostr.html" },
   { label: "Jack article", name: "naddr1qqxnzd3cxyerxd3h8qerwwfcqgsgydql3q4ka27d9wnlrmus4tvkrnc8ftc4h8h5fgyln54gl0a7dgsrqsqqqa28387u5u"},
   { label: "Tony article", name: "naddr1qqxnzd3cxy6rjv3hx5cnyde5qy88wumn8ghj7mn0wvhxcmmv9uq3uamnwvaz7tmwdaehgu3dwp6kytnhv4kxcmmjv3jhytnwv46z7qg3waehxw309ahx7um5wgh8w6twv5hszymhwden5te0danxvcmgv95kutnsw43z7qglwaehxw309ahx7um5wgkhyetvv9ujumn0ddhhgctjduhxxmmd9upzql6u9d8y3g8flm9x8frtz0xmsfyf7spq8xxkpgs8p2tge25p346aqvzqqqr4gukz494x"},
   { label: "Cholesterol tag bug", name: "naddr1qq2kj3nvwf2xsh63dyc8sm35xdy8q7282pgkjq3qtta8zx3wfazjjnyu4qpnscdqu9dg08n0cxj2dypkgrml46hq67uqxpqqqp65wwk6pht"},
   { label: "some note", name: "note1nndua46su6c7zf6t0h68edpn74j5znws3jyhzelt5as74hhgg8xq4qmzwn"},
   { label: "highlight", name: "nevent1qqsprhvdfau2ezh6mjpess9g5v6g9c657j99jke04s3hc7xrv4vve4qzypl62m6ad932k83u6sjwwkxrqq4cve0hkrvdem5la83g34m4rtqegx3l8d3"}
 ];
-
-const format = (item, type) => (type === "option" ? item.label : item.name);
 
 const relays = ["wss://relay.damus.io", "wss://eden.nostr.land"];
 const otherRelays = ['wss://relay.damus.io', 'wss://nostr.mom', 'wss://nos.lol']
@@ -37,7 +36,7 @@ const altPubkey = "afd563434a737334d69db899e4a32fe38d73a182bb6d1e91d83a2c4c4e047
 
 render(() => {  
   const [pubkey, setPubkey] = createSignal(defaultPubkey);
-  const [anchor, setAnchor] = createSignal(options[2].name);
+  const [anchor, setAnchor] = createSignal('');
   
   return <>
     <h1>Super Blog</h1>
@@ -48,8 +47,9 @@ render(() => {
     <button onClick={() => pubkey() ? setPubkey('') : setPubkey(defaultPubkey)}>{pubkey() ? 'Log out' : 'Log in'}</button>
     <hr/>
 
-    <Select class="custom" initialValue={options[2]} options={options} format={format} onChange={(e) => setAnchor(e.name)} />
+    <Select class="custom" initialValue={options[0]} options={options} format={(item) => item.label} onChange={(e) => setAnchor(e.name)} />
 
+    <hr/>
     <p>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     </p>
--- src/thread.tsx
+++ src/thread.tsx
@@ -4,6 +4,7 @@ import { ReplyEditor } from "./reply";
 import { NestedNote } from "./util/nest";
 import { StoredProfile, ZapThreadsContext } from "./util/stores";
 import { npubEncode } from "./nostr-tools/nip19";
+import { createElementSize } from "@solid-primitives/resize-observer";
 
 export const Thread = (props: { nestedEvents: () => NestedNote[]; }) => {
   const { anchor, preferencesStore, profiles } = useContext(ZapThreadsContext)!;
@@ -13,14 +14,28 @@ export const Thread = (props: { nestedEvents: () => NestedNote[]; }) => {
       {
         (event) => {
           const [isOpen, setOpen] = createSignal(false);
+          const [isExpanded, setExpanded] = createSignal(false);
           const infoSignal = createSignal(false);
           const [showInfo, setShowInfo] = infoSignal;
+
+          const MAX_HEIGHT = 500;
+          const [target, setTarget] = createSignal<HTMLElement>();
+          const size = createElementSize(target);
 
           return <div class="ztr-comment">
             <div class="ztr-comment-body">
               <CommentInfo event={event} profiles={profiles()} infoSignal={infoSignal} />
-              <div class="ztr-comment-text" innerHTML={parseContent(event(), profiles(), anchor(), preferencesStore)}>
+              <div ref={setTarget} class="ztr-comment-text" style={!isExpanded() ? { 'max-height': `${MAX_HEIGHT}px` } : {}} innerHTML={parseContent(event(), profiles(), anchor(), preferencesStore)}>
               </div>
+
+              {size.height && size.height >= MAX_HEIGHT && !isExpanded() &&
+                <div class="ztr-comment-expand">
+                  <a style={{ 'height': `${svgWidth}px` }}>
+                    <svg width={svgWidth} height={svgWidth} viewBox="0 0 576 512"><path d="M168 80c-13.3 0-24 10.7-24 24V408c0 8.4-1.4 16.5-4.1 24H440c13.3 0 24-10.7 24-24V104c0-13.3-10.7-24-24-24H168zM72 480c-39.8 0-72-32.2-72-72V112C0 98.7 10.7 88 24 88s24 10.7 24 24V408c0 13.3 10.7 24 24 24s24-10.7 24-24V104c0-39.8 32.2-72 72-72H440c39.8 0 72 32.2 72 72V408c0 39.8-32.2 72-72 72H72zM176 136c0-13.3 10.7-24 24-24h96c13.3 0 24 10.7 24 24v80c0 13.3-10.7 24-24 24H200c-13.3 0-24-10.7-24-24V136zm200-24h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H376c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H376c-13.3 0-24-10.7-24-24s10.7-24 24-24zM200 272H408c13.3 0 24 10.7 24 24s-10.7 24-24 24H200c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80H408c13.3 0 24 10.7 24 24s-10.7 24-24 24H200c-13.3 0-24-10.7-24-24s10.7-24 24-24z" /></svg>
+                  </a>
+                  <span onClick={() => setExpanded(true)}>Show full comment</span>
+                </div>}
+
               <ul class="ztr-comment-actions">
                 {/* <Show when={!preferencesStore.disableZaps}>
                   <li class="ztr-comment-action-zap">
