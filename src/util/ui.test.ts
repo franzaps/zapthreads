4a48620d17b88e1ecfb699056e00013cf24f4bf7
franzap <_@franzap.com>
2023-10-01 03:52:17 -0300

    fix test

--- src/util/ui.test.ts
+++ src/util/ui.test.ts
@@ -3,11 +3,15 @@ import { parseContent, parseUrlPrefixes } from "./ui";
 
 describe("ui utils", () => {
   describe("parseContent", () => {
-    it('removes naddr if same', async () => {
+    it('removes naddr if mentioned', async () => {
       const naddr = "naddr1qqxnzd3cxqmrzv3exgmr2wfeqyf8wumn8ghj7ur4wfcxcetsv9njuetnqyxhwumn8ghj7mn0wvhxcmmvqy08wumn8ghj7mn0wd68yttjv4kxz7fwdehkkmm5v9ex7tnrdakszynhwden5te0danxvcmgv95kutnsw43qz9rhwden5te0wfjkccte9ejxzmt4wvhxjmcpzpmhxue69uhkummnw3ezuamfdejsygrwg6zz9hahfftnsup23q3mnv5pdz46hpj4l2ktdpfu6rhpthhwjvpsgqqqw4rskylmpy";
       const e: UnsignedEvent = {
         "kind": 1,
-        "tags": [],
+        "tags": [
+          [
+            'a', '30023:6e468422dfb74a5738702a8823b9b28168abab8655faacb6853cd0ee15deee93:1680612926599', '', 'mention'
+          ]
+        ],
         "created_at": 0,
         "pubkey": "",
         "content": `awesome article\n nostr:${naddr}`
